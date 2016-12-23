e8ef2ecebde6b7a609798de0bc44eeb97bb52658
Gabi Procell <gprocell927@gmail.com>
2016-12-23 16:57:19 -0700

    Creates data object to use for form

---
+++ data-object.js
@@ -0,0 +1,47 @@
+{"procedure": [
+  {"surgeon": "Justin Starnes, DVM"}
+  {"assistant": "Jenna Smith"}
+  {"anesthetist": "Gabi Procell, CVT"}
+  {"pa-dx": "CCL rupture"}
+  {"proposed-procedure": "Cruciate repair left hind limb"}
+]},
+{"patient-info": [
+  {"name": "Dori Ramirez"}
+  {"DOB": "10/12/2008"}
+  {"Species": "Canine"}
+  {"Sex": "FS"}
+  {"PA Warnings": "none"}
+  {"Behavioural warnings": "none"}
+]},
+{"Pre-anesthetic vitals": [
+  {"Body weight": "25 kg"}
+  {"Temperature": "101.0"}
+  {"Pulse": "89"}
+  {"Respiration": "15"}
+  {"MM": "pink and moist"}
+  {"CRT": "< 2 seconds"}
+  {"Physical status": "2"}
+  {"Drugs within last 24 hours": "Denamarin"}
+]},
+{"Drugs Administered": [
+  {"PA Meds": "Dexdomitor 0.25 mL IM LPL 9:40 AM"}
+  {"PA Meds": "Hydromorphone 0.2 mL IM RPL 9:40 AM"}
+  {"Induction Meds": "Propofol 6 mL IV 10:00 AM"}
+  {"ET Tube Size": "10 cm"}
+]},
+{"Readings": [
+  {"Time": "10:05 AM"}
+  {"Vaporizer": "2"}
+  {"Oxygen": "2.0"}
+  {"Pulse" : "86"}
+  {"Respiration": "20"}
+  {"Systolic BP": "100"}
+  {"Diastolic BP": "70"}
+  {"Mean BP": "89"}
+  {"ETCO2": "40"}
+  {"SPO2": "99"}
+]},
+{"Post surgery": [
+  {"Temperature": "100"}
+  {"Total fluids": "750 mL"}
+]}
