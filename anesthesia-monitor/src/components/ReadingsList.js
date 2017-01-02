79a8613bd8e691ac6a67f7e05e2dbce15b25f0f7
Gabi Procell <gprocell927@gmail.com>
2017-01-01 19:29:34 -0700

    Adds a ReadingsList component that will render a list of readings from the state

---
+++ anesthesia-monitor/src/components/ReadingsList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Reading from './Reading'
+
+const ReadingsList = ({ readings, onReadingClick }) => {
+  if (readings.length === 0){
+    return <h3> There are no saved readings for this procedure.</h3>
+  }
+
+  return(
+    <ul>
+      { readings.map(reading => {
+        return (
+          <Reading
+            key={reading.id}
+            {...reading}
+            handleClick={() => onReadingClick(reading.id)}
+          />
+        )
+      }) }
+    </ul>
+  )
+}
+
+export default ReadingsList
