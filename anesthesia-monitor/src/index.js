675afb4bd4d82f82834f8e229ea4d53ff9243a82
Gabi Procell <gprocell927@gmail.com>
2017-01-04 08:50:58 -0700

    sets up firebase in redux store

--- anesthesia-monitor/src/index.js
+++ anesthesia-monitor/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers'
+import { createStore, compose } from 'redux';
+import { reactReduxFirebase } from 'react-redux-firebase';
+import reducers from './reducers';
 import App from './components/App';
 import './index.css';
 
-let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const config = {
+  apiKey: "AIzaSyAYGEL7PNQHFLObNrpc8dpKpvCHXY59zSE",
+  authDomain: "anesthesiaapp-337a3.firebaseapp.com",
+  databaseURL: "https://anesthesiaapp-337a3.firebaseio.com",
+  storageBucket: "anesthesiaapp-337a3.appspot.com",
+  messagingSenderId: "634025903164"
+}
+
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(config, { reading: 'readings' }),)(createStore)
+
+let store = createStoreWithFirebase(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
 
 
 render(
