0c08715f6ea85f98ab8879a83e3283b19141d9c0
Gabi Procell <gprocell927@gmail.com>
2017-01-04 08:35:07 -0700

    adds react-redux-firebase to dependencies and sets up firebase reducer

--- anesthesia-monitor/src/reducers/index.js
+++ anesthesia-monitor/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
+import { firebaseStateReducer as firebase } from 'react-redux-firebase'
 import readings from './readings'
 import setFilter from './setFilter'
 
 const reducers = combineReducers({
   readings,
-  setFilter
+  setFilter,
+  firebase
 })
 
 export default reducers
